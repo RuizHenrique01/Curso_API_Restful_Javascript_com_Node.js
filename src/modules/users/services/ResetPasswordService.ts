167fd8980d699ea7f7300cd20383c716fb6cb410
RuizHenrique01 <affonsohenriqueruiz@gmail.com>
2021-10-05 13:54:51 -0400

    adicionando serviço de resetar senha

---
+++ src/modules/users/services/ResetPasswordService.ts
@@ -0,0 +1,40 @@
+import AppError from '@shared/errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import { isAfter, addHours } from 'date-fns';
+import { hash } from 'bcryptjs';
+import UsersRepository from '../typeorm/repositories/UsersRepository';
+import UserTokensRepository from '../typeorm/repositories/UserTokensRepository';
+
+interface IRequest {
+    token: string;
+    password: string;
+}
+
+class ResetPasswordEmailService {
+    public async execute({ token, password }: IRequest): Promise<void> {
+        const userRepository = getCustomRepository(UsersRepository);
+        const userTokensRepository = getCustomRepository(UserTokensRepository);
+
+        const userToken = await userTokensRepository.findByToken(token);
+
+        if (!userToken) {
+            throw new AppError('User Token does not exists.');
+        }
+
+        const user = await userRepository.findById(userToken.user_id);
+
+        if (!user) {
+            throw new AppError('User does not exists.');
+        }
+
+        const compareDate = addHours(userToken.created_at, 2);
+
+        if (isAfter(Date.now(), compareDate)) {
+            throw new AppError('Token expired.');
+        }
+
+        user.password = await hash(password, 8);
+    }
+}
+
+export default ResetPasswordEmailService;
