43645bfd3155af2cb9be4e78eb31cb21f5eb5703
RuizHenrique01 <affonsohenriqueruiz@gmail.com>
2021-10-18 10:43:19 -0400

    adicionando serviços de Perfil de Usuário

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -3,6 +3,7 @@ import productRouter from '@modules/products/routes/products.router';
 import usersRouter from '@modules/users/routes/users.router';
 import sessionsRouter from '@modules/users/routes/sessions.router';
 import passwordRouter from '@modules/users/routes/password.router';
+import profileRouter from '@modules/users/routes/profile.router';
 
 const routes = express.Router();
 
@@ -10,5 +11,6 @@ routes.use('/products', productRouter);
 routes.use('/users', usersRouter);
 routes.use('/sessions', sessionsRouter);
 routes.use('/password', passwordRouter);
+routes.use('/profile', profileRouter);
 
 export default routes;
